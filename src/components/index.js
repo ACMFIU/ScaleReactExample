d43802874d05ac6a1491120ea23f9b551b1d2f86
Fernando Serrano <fserr009@fiu.edu>
2020-03-06 13:16:58 -0500

    cleaned up and added packages and removed some unneeded boiler plate files

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Container} from 'react-bootstrap';
+
+class Main extends React.Component {
+  render(){
+    return(
+      <Container>
+      </Container>
+    );
+  }
+}
+
+export default Main;
